e3e1ff87b1115184e3427589850c5e6e6c76109f
slayeryx <slayermaster211@gmail.com>
2026-05-22 23:20:45 -0400

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,5 @@
+local Games = {
+  [6447798030] = "https://raw.githubusercontent.com/slayeryx/Script-Games/refs/heads/main/fnf.lua?token=GHSAT0AAAAAAD43G4LOOL4M7JWMTJI4J2AM2QRDUOA", -- Funky-Friday
+}
+
+return Games
