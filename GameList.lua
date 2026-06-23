9ea21b57cd129529c3f39fe253314ac29409d89a
slayeryx <slayermaster211@gmail.com>
2026-06-23 08:44:44 -0400

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
-  [6447798030] = "https://raw.githubusercontent.com/slayeryx/Script-Games/refs/heads/main/fnf.lua?token=GHSAT0AAAAAAD43G4LOOL4M7JWMTJI4J2AM2QRDUOA", -- Funky-Friday
-  [79268393072444] = "https://raw.githubusercontent.com/slayeryx/Script-Games/refs/heads/main/Sell%20Lemons?token=GHSAT0AAAAAAD43G4LOSTLT6T46VMPKCURO2RYO2XA", -- Sell Lemons
+  [6447798030] = "https://raw.githubusercontent.com/slayeryx/Script-Games/refs/heads/main/fnf.lua", -- Funky-Friday
+  [79268393072444] = "https://raw.githubusercontent.com/slayeryx/Script-Games/refs/heads/main/Sell%20Lemons", -- Sell Lemons
 }
 
 return Games
