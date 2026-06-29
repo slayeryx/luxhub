cb6d13b4c7e083dea0b4aa044ddff27ab17a5caf
slayeryx <slayermaster211@gmail.com>
2026-06-29 18:25:05 -0400

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,7 @@
 local Games = {
   [6447798030] = "https://raw.githubusercontent.com/slayeryx/ScriptGames/refs/heads/main/fnf.lua", -- Funky-Friday
   [79268393072444] = "https://raw.githubusercontent.com/slayeryx/ScriptGames/refs/heads/main/Sell%20Lemons", -- Sell Lemons
-  [6839171747] = "https://raw.githubusercontent.com/slayeryx/ScriptGames/refs/heads/main/Door/Loader.luau", -- Doors
+  [6839171747] = "https://raw.githubusercontent.com/deadratboy/script/refs/heads/main/Loader.luau", -- Doors
 }
 
 return Games
